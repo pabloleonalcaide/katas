342844906b43e399b1e499bef5d0fddaf9bde018
Pablo Leon <pabloleonpsico@gmail.com>
2020-09-30 12:20:16 +0200

    Change inmutable values to const

--- greeting kata/test/greeting.test.ts
+++ greeting kata/test/greeting.test.ts
@@ -2,50 +2,50 @@ import {Greeter} from '../src/greeting';
 import { Group } from '../src/group';
 
 describe('Greeting Kata', () => {
-    const greeter = new Greeter();
+  const greeter = new Greeter();
 
   test('Greet a single name', () => { 
-    let expectedResponse = 'Hello, Bob.'
+    const expectedResponse = 'Hello, Bob.'
     expect(greeter.greet('Bob')).toBe(expectedResponse);
   })
 
   test('Handle nulls by using a stand-in', () => {
-    let expectedResponse = 'Hello, my friend.'
+    const expectedResponse = 'Hello, my friend.'
     expect(greeter.greet(null)).toBe(expectedResponse);
   })
 
   test('Handle shouting', () => {
-    let expectedResponse = 'HELLO, BOB.'
+    const expectedResponse = 'HELLO, BOB.'
     expect(greeter.greet('BOB')).toBe(expectedResponse);
   })
 
   test('Handle two names', () => {
-    let expectedResponse = 'Hello, Bob and Wyde'
-    let group = new Group(['Bob', 'Wyde']);
+    const expectedResponse = 'Hello, Bob and Wyde'
+    const group = new Group(['Bob', 'Wyde']);
     expect(greeter.greet(group)).toBe(expectedResponse);
   })
 
   test('Handle more than two names', () => {
-    let expectedResponse = 'Hello, Bob, Logan and Wyde'
-    let group = new Group(['Bob', 'Logan', 'Wyde']);
+    const expectedResponse = 'Hello, Bob, Logan and Wyde'
+    const group = new Group(['Bob', 'Logan', 'Wyde']);
     expect(greeter.greet(group)).toBe(expectedResponse);
   })
 
   test('Handle mixing of normal and shouted names', () => {
-    let expectedResponse = 'Hello, Amy and Charlotte. AND HELLO BRIAN!'
-    let group = new Group(['Amy', 'BRIAN', 'Charlotte']);
+    const expectedResponse = 'Hello, Amy and Charlotte. AND HELLO BRIAN!'
+    const group = new Group(['Amy', 'BRIAN', 'Charlotte']);
     expect(greeter.greet(group)).toBe(expectedResponse);
   })
 
   test('Split names with commas into separate entries', () => {
-    let expectedResponse = 'Hello, Bob, Charlie, and Dianne';
-    let group = new Group(['Bob','Charlie, Dianne']);
+    const expectedResponse = 'Hello, Bob, Charlie, and Dianne';
+    const group = new Group(['Bob','Charlie, Dianne']);
     expect(greeter.greet(group)).toBe(expectedResponse);
   })
 
   test('Allow commas in entries to be escapes', () => {
-    let expectedResponse = 'Hello, Bob and Charlie, Dianne';
-    let group = new Group(['Bob', '"Charlie,Dianne"']);
+    const expectedResponse = 'Hello, Bob and Charlie, Dianne';
+    const group = new Group(['Bob', '"Charlie,Dianne"']);
     expect(greeter.greet(group)).toBe(expectedResponse);
   })
 })
